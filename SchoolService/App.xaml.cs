d023ff0aca063bb3a9d56b36d1e0abc9fb929a4f
Madrid-Born <alirezanoori.1381@gmail.com>
2024-01-28 23:30:27 +0330

    first

--- SchoolService/App.xaml.cs
+++ SchoolService/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace SchoolService
+﻿using SchoolService.Pages;
+
+namespace SchoolService
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new InitialPage());
         }
     }
 }
