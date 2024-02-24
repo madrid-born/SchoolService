7fc7367a2bd6d6e4f99ed12cc5d9e5db2c538f04
mohamad801114 <zareim003@gmail.com>
2024-02-24 22:55:33 +0330

    5/12

---
+++ SignalR/SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace SignalR.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            Console.WriteLine(message);
+            await Clients.All.SendAsync("Recive Message", message);
+        }
+    }
+}
